35a6b4c521c2c0dccc36035bf8157e509a7205de
mohammadkabbara <mohammadkabbara40@gmail.com>
2021-07-08 00:39:25 +0300

    practice

--- practice2.js
+++ practice2.js
@@ -1,9 +1,27 @@
 
 
     
-$(document).ready(function()
-{
-    $('button').click(function(){
-        $("p").hide();
-    })
-});
+// $(document).ready(function()
+// {
+//     $('button').click(function(){
+//         $("p").fadeIn(5000);
+//     })
+// });
+
+
+// $(document).ready(function(){
+  
+//         $("span").animate({width:"500px" , left:"50px" ,hight: "20px",backgroundColor:"red" },5000)
+  
+// })
+
+$(document).ready(function(){
+    
+        $("p").animate({top:'10px'},1000);
+        $("p").animate({left:'500px'},1000);
+        $("p").animate({height:'200px'},1000);
+        $("p").animate({width:'600px'},1000);
+        $("p").animate({borderRadius:'50px'},1000);
+        $("p").animate({fontSize:'50px'},1000);
+        $("p").fadeOut(4000);
+    });
